27bd9b55d3ca5257e2be12a18589a314765656cd
ocean <145195245+nesyass@users.noreply.github.com>
2024-12-15 19:07:36 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ int main()
     int pilih;
     menu();
     cin >> pilih;
+    cin.ignore();
 
     while (pilih >= 1 && pilih < 17) {
         clearScreen();
@@ -28,22 +29,25 @@ int main()
             cout << "Masukkan jumlah aplikasi yang ingin ditambahkan: ";
             int c;
             cin >> c;
+            cin.ignore();
             for (int i = 0; i < c; i++) {
                 cout << endl;
                 cout << "Nama Aplikasi: ";
-                IA.nama_aplikasi = inputanKalimat();
+                getline(cin, IA.nama_aplikasi);
                 // Cek apakah aplikasi sudah ada
                 if (findAplikasi(LA, IA.nama_aplikasi) != NULL) {
                     cout << "Aplikasi dengan nama \"" << IA.nama_aplikasi << "\" sudah ada. Tidak dapat ditambahkan lagi." << endl;
                 } else {
                     cout << "Kualitas Video dalam Aplikasi: ";
-                    cin >> IA.kualitas;
+                    getline(cin, IA.kualitas);
 
                     cout << "Rating Aplikasi: ";
                     cin >> IA.rating;
+                    cin.ignore();
 
                     cout << "Harga Langganan Bulanan: ";
                     cin >> IA.harga;
+                    cin.ignore();
 
                     PA = allocateAplikasi(IA);
                     insertAplikasi(LA, PA);
@@ -54,6 +58,7 @@ int main()
             cout << endl;
             menu();
             cin >> pilih;
+            cin.ignore();
         } else if (pilih == 2) {
             cout << ">>> Tambah Film <<<" << endl;
             address_film PF;
@@ -61,20 +66,23 @@ int main()
             cout << "Masukkan jumlah film yang ingin ditambahkan: ";
             int c;
             cin >> c;
+            cin.ignore();
             for (int i = 0; i < c; i++) {
                 cout << endl;
                 cout << "Nama Film: ";
-                IF.nama = inputanKalimat();
+                getline(cin, IF.nama);
                 // Cek apakah film sudah ada
                 if (findFilm(LF, IF.nama) != NULL) {
                     cout << "Film dengan nama \"" << IF.nama << "\" sudah ada. Tidak dapat ditambahkan lagi." << endl;
                 } else {
                     cout << "Genre: ";
-                    cin >> IF.genre;
+                    getline(cin, IF.genre);
                     cout << "Tahun Rilis: ";
                     cin >> IF.tahun;
+                    cin.ignore();
                     cout << "Rating: ";
                     cin >> IF.rating;
+                    cin.ignore();
                     PF = allocateFilm(IF);
                     insertFilm(LF, PF);
                     cout << endl;
@@ -83,123 +91,139 @@ int main()
             }
             menu();
             cin >> pilih;
+            cin.ignore();
         } else if (pilih == 3) {
             cout << ">>> Tampilkan Semua Aplikasi <<<" << endl;
             showAllAplikasi(LA);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 4) {
             cout << ">>> Tampilkan Semua Film <<<" << endl;
             showAllFilm(LF);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 5) {
             cout << ">>> Buat Relasi Aplikasi dengan Film <<<" << endl;
             cout << "Nama Aplikasi: ";
-            cin >> nama_app;
+            getline(cin, nama_app);
             cout << "Nama Film: ";
-            cin >> nama_film;
+            getline(cin, nama_film);
             insertRelation(LA, LF, nama_app, nama_film);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 6) {
             cout << ">>> Tampilkan Daftar Film dari Aplikasi <<<" << endl;
             cout << "Nama Aplikasi: ";
-            cin >> nama_app;
+            getline(cin, nama_app);
             cout << endl;
             showChildFromParent(LA, nama_app);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 7) {
             cout << ">>> Tampilkan Daftar Aplikasi dari Film <<<" << endl;
             cout << "Nama Film: ";
-            cin >> nama_film;
+            getline(cin, nama_film);
             cout << endl;
             showParentsFromChild(LF, LA, nama_film);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 8) {
             cout << ">>> Hapus Aplikasi <<<" << endl;
             cout << "Nama Aplikasi: ";
-            cin >> nama_app;
+            getline(cin, nama_app);
             deleteAplikasi(LA, LF, nama_app);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 9) {
             cout << ">>> Hapus Film <<<" << endl;
             cout << "Nama Film: ";
-            cin >> nama_film;
+            getline(cin, nama_film);
             deleteFilm(LF, LA, nama_film);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 10) {
             cout << ">>> Hapus Relasi <<<" << endl;
             cout << "Nama Aplikasi: ";
-            cin >> nama_app;
+            getline(cin, nama_app);
             cout << "Nama Film: ";
-            cin >> nama_film;
+            getline(cin, nama_film);
             deleteRelation(LA, nama_app, nama_film);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 11) {
             cout << ">>> Show Data Film dan Aplikasi <<<" << endl;
-            showChildWithParents(LF, LA); //tampilan kalo kosong masih jelek
+            showChildWithParents(LF, LA);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 12) {
             cout << ">>> Show Data Aplikasi dengan Film <<<" << endl;
             showParentWithChild(LA);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 13) {
             cout << ">>> Hitung Relasi dari Aplikasi <<<" << endl;
             countRelationFromParent(LA);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 14) {
             cout << ">>> Hitung Relasi dari Film <<<" << endl;
             cout << "Nama Film: ";
-            cin >> nama_film;
+            getline(cin, nama_film);
+            cout << endl;
             countRelationFromChild(LF, LA, nama_film);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 15) {
             cout << ">>> Hitung Film Tanpa Relasi <<<" << endl;
             countChildWithoutRelation(LF, LA);
             menu();
             cin >> pilih;
+            cin.ignore();
 
         } else if (pilih == 16) {
             cout << ">>> Edit Relasi Aplikasi <<<" << endl;
             cout << "Nama Aplikasi: ";
-            cin >> nama_app;
+            getline(cin, nama_app);
             string nama_film_baru;
             cout << "Nama Film Baru: ";
-            cin >> nama_film_baru;
+            getline(cin, nama_film_baru);
             cout << "Nama Film Lama: ";
-            cin >> nama_film;
+            getline(cin, nama_film);
             editRelation(LA, LF, nama_app, nama_film, nama_film_baru);
             menu();
             cin >> pilih;
+            cin.ignore();
         }
     }
 
     if (pilih >= 17 || pilih <= 0) {
         clearScreen();
         cout << "=================================================================" << endl;
-        cout << "                     ANDA KELAR DARI PROGRAM          " << endl;
+        cout << "                     ANDA KELUAR DARI PROGRAM          " << endl;
         cout << "=================================================================" << endl;
     }
 
